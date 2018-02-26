ee58cf857eae14c43605997cd42319a15e90f3b6
THULiusj <shliup@microsoft.com>
2018-02-26 14:17:49 +0800

    Update readme English version

--- dataprocess.py
+++ dataprocess.py
@@ -9,20 +9,11 @@ import argparse
 import matplotlib.pyplot as plt
 from PIL import Image
 
-def get_Directory_Name(path):
-    dirNameList = path.split("/")
-    directory = "/".join(dirNameList[0:len(dirNameList)-1])
-    name = dirNameList[-1]
-    print(path)
-    print(name)
-    print(directory)
-    return (name, directory)
-
 IM_SIZE = (299, 299)
 
-data = np.loadtxt("./CUB_200_2011/train_test_split.txt", delimiter=' ')
+data = np.loadtxt("train_test_split.txt", delimiter=' ')
 data = np.array(data, dtype=int)
-imglist = np.genfromtxt('./CUB_200_2011/images.txt',delimiter=' ',dtype='str')
+imglist = np.genfromtxt('images.txt',delimiter=' ',dtype='str')
 
 X_train = []
 y_train = []
@@ -30,27 +21,25 @@ X_test=[]
 y_test=[]
 for imgid, flag in data:
    if flag == 0:
-     X_train.append('./CUB_200_2011/images/'+ imglist[imgid-1, 1])
+     X_train.append('images/'+ imglist[imgid-1, 1])
      y_train.append(imglist[imgid-1, 1].split("/",1)[0] )
    else:
-     X_test.append('./CUB_200_2011/images/' + imglist[imgid-1, 1])
+     X_test.append('images/' + imglist[imgid-1, 1])
      y_test.append(imglist[imgid-1, 1].split("/",1)[0] )
 
 # save the images to train dataset and test dataset
 for imgpath in X_train:
     im = Image.open(imgpath)
     #im.thumbnail(IM_SIZE)
-    (name, directory) = get_Directory_Name(imgpath)
-    savepath = './train_images/'+ directory
+    savepath = 'train_images/'+ imgpath.split("/",1)[1].split("/",1)[0]
     if not os.path.exists(savepath):
         os.makedirs(savepath)
-    im.save(savepath + '/'+ name)
+    im.save(savepath + '/'+ imgpath.split("/",1)[1].split("/",1)[1])
 
 for imgpath in X_test:
-    im = Image.open(imgpath)
-    #im.thumbnail(IM_SIZE)
-    (name, directory) = get_Directory_Name(imgpath)
-    savepath = './test_images/'+ directory
-    if not os.path.exists(savepath):
-      os.makedirs(savepath)
-    im.save(savepath + '/'+ name)
+   im = Image.open(imgpath)
+   #im.thumbnail(IM_SIZE)
+   savepath = 'test_images/'+ imgpath.split("/",1)[1].split("/",1)[0]
+   if not os.path.exists(savepath):
+     os.makedirs(savepath)
+   im.save(savepath + '/'+ imgpath.split("/",1)[1].split("/",1)[1])
--- readme.md
+++ readme.md
@@ -13,42 +13,24 @@
 1) 创建Azure 数据科学虚拟机
 在全球版Azure的管理门户上搜索Azure数据科学虚拟机(Data Science Virtual Machine)，开始创建。注意选择Ubuntu系统，HDD磁盘类型，NC6型号虚拟机。创建成功后，通过Putty连接虚拟机。
 
-2) 训练环境配置
+3) 训练环境配置
 配置Keras默认后台为Tensorflow。也可以在/home/<username>/.keras里面找到keras.json文件。
 如果使用Python环境运行代码，运行source activate py35，启动虚拟环境。
 
-3) 下载示例代码
-
-```
-git clone https://github.com/MS-CSE-GCR/BirdClassification-Keras-Tensorflow-Inception.git
-cd BirdClassification-Keras-Tensorflow-Inception
-```
-
-4) 下载数据集并解压
-
-```
-wget http://www.vision.caltech.edu/visipedia-data/CUB-200-2011/CUB_200_2011.tgz
-tar xvf CUB_200_2011.tgz
-```
-
-5) 分别运行示例代码
-```
-python dataprocess.py
-python train.py
-python test.py <URL of Image>
-```
-
 ## 数据说明
 数据是来自[Caltech-UCSD Birds-200-2011 (CUB-200-2011)](http://www.vision.caltech.edu/visipedia/CUB-200-2011.html)，包含200种鸟类的图片，可以用于进行鸟的识别、部位检测和图像分割。[下载](http://www.vision.caltech.edu/visipedia-data/CUB-200-2011/CUB_200_2011.tgz)
+
 <img src="image/CUBshot.png" width="600" height="280" />
 
 ## 参考来源
 -  https://github.com/DeepLearningSandbox/DeepLearningSandbox/blob/master/transfer_learning
 
 ## 代码说明
+- 所有代码文件需放在包含images和attributes文件夹这一层目录下面。
 - dataprocess.py: 数据预处理，拆分训练集和测试集。
 - train.py: 数据增强，模型训练。
         
    - 选用50%数据训练时，训练集Accuracy约为96%，验证Accuracy约为70%
    - 所有数据集都作为训练集时，Accuracy约为91%
 - test.py: 基于训练好的模型进行网络图片测试。运行方式: python test.py "Internet img url"
+
